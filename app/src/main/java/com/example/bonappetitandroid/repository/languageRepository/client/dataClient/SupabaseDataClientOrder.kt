ea206231c71cdcc46b8e3a7c3708d46f452e824b
Bacya <bacyadaniil@gmail.com>
2023-04-09 19:16:01 +0300

    updated files

--- app/src/main/java/com/example/bonappetitandroid/repository/languageRepository/client/dataClient/SupabaseDataClientOrder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bonappetitandroid.repository.languageRepository.client.dataClient
-
-import com.example.bonappetitandroid.dto.OrderInfo
-
-interface SupabaseDataClientOrder {
-    suspend fun getOrderData(): List<OrderInfo>
-    suspend fun addOrderData()
-}
--- app/src/main/java/com/example/bonappetitandroid/repository/languageRepository/client/dataClient/SupabaseDataClientProfile.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bonappetitandroid.repository.languageRepository.client.dataClient
-
-import com.example.bonappetitandroid.dto.ProfileInfo
-
-interface SupabaseDataClientProfile {
-    suspend fun getProfileData(): List<ProfileInfo>
-    suspend fun addProfileData()
-    suspend fun deleteProfileData()
-}
